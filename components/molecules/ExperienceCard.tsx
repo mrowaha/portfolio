106624d815f0dc56c7a258872e05c4b5da01ebbc
mrowaha <ashfaqrowaha@gmail.com>
2024-02-19 17:29:49 +0300

    add experience cards

--- components/molecules/ExperienceCard.tsx
+++ components/molecules/ExperienceCard.tsx
@@ -1,24 +1,37 @@
+import * as React from "react";
 import Image from "next/image";
 
 import {
-  Card,
-  CardContent,
   Typography,
-  useTheme
+  Stack,
+  useTheme,
+  Divider,
+  Accordion,
+  List,
+  ListItem,
+  ListItemText
 } from "@mui/material";
+import AccordionSummary from '@mui/material/AccordionSummary';
+import AccordionDetails from '@mui/material/AccordionDetails';
+import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
 import {styled} from "@mui/system";
 
-const StyledCard = styled(Card)(({theme}) => ({
+const StyledCard = styled(Stack)(({theme}) => ({
   background : "linear-gradient(#dfdfdf80, #dfdfdfff)",
   border :"2px solid #dfdfdfff", 
   padding : "1rem",
-  alignItems : "center"
+  alignItems : "center",
+  width: "100%",
+  // @ts-ignore
+  alignItems: "flex-start"
 }))
 
 export interface ExperienceCardProps {
   img: number;
   role: string;
   company: string;
+  date: string;
+  work: string[];
 }
 
 
@@ -27,27 +40,49 @@ export default function ExperienceCard(props: ExperienceCardProps) {
   const theme = useTheme();
 
   return (
-  <StyledCard>
-    <div style={{width : "100%", display : "flex", justifyContent : "center"}}>
-      <Image 
-        src={`/experience/${props.img}.png`}
-        alt="experience logo"
-        width={140}
-        height={140}
-        style={{border : `3px solid ${theme.palette.secondary.main}`}}
-      />               
-    </div>
-    <CardContent>
-      <Typography gutterBottom variant="h5" component="div" color="primary" textAlign="center">
-        {props.role}
-      </Typography>
-      <Typography style={{color : theme.palette.mode === 'dark' ? "#dfdfdfff" : "#313131aa"}} >
-        <span style={{fontWeight : "bold"}} >At: </span>{props.company}
-      </Typography>
-      <Typography style={{color : theme.palette.mode === 'dark' ? "#dfdfdfff" : "#313131aa"}} fontWeight="bold">
-        Work:
-      </Typography>
-    </CardContent>
-  </StyledCard>
+    <StyledCard direction={"row"} gap={5}>
+      <div>
+        <Image 
+          src={`/experience/${props.img}.png`}
+          alt="experience logo"
+          width={140}
+          height={140}
+          style={{border : `3px solid ${theme.palette.secondary.main}`, borderRadius: "10px"}}
+        />               
+      </div>
+      <Divider orientation="vertical" />
+      <div style={{flexGrow: 1}}>
+        <Typography gutterBottom variant="h5" color="primary" textAlign="center" sx={{width: "fit-content"}}>
+          {props.role}
+        </Typography>
+        <Typography style={{color : theme.palette.secondary.main, textTransform: "uppercase", fontWeight: "bold"}} >
+          {props.company}
+        </Typography>
+        <Typography style={{color : theme.palette.mode === 'dark' ? "#dfdfdfff" : "#313131aa"}} >
+          {props.date}
+        </Typography>
+        <Accordion sx={{background: "linear-gradient(90deg, #690169, #d6023b)"}}>
+          <AccordionSummary
+            expandIcon={<ExpandMoreIcon style={{fill: "#fff"}}/>}
+          >
+          <Typography variant="h6" sx={{color: "#fff"}}>Details</Typography>
+          </AccordionSummary>
+          <AccordionDetails>
+            <List>
+              {
+                React.Children.toArray(
+                  props.work.map(work => 
+                  <ListItem>
+                    <ListItemText 
+                      primary={<Typography color="#fff">{work}</Typography>}
+                    />
+                  </ListItem>)
+                )
+              }
+            </List>
+          </AccordionDetails>
+        </Accordion>
+      </div>
+    </StyledCard>
   )
 }
--- pages/experience.tsx
+++ pages/experience.tsx
@@ -16,7 +16,38 @@ const experiences : ExperienceCardProps[] = [
   {
     img: 0,
     company: "Layermark",
-    role: "Software Developer"
+    role: "Software Developer",
+    date: "April 2023 - Feb 2024",
+    work: [
+      "Developed user interfaces with DevExtreme and AntDesign for  web  and  native  applications  meeting  specific industrial settings.",
+      "Developed Python modules-Opencv video processing, pdfgeneration, Yolov5 ai models, and WebSockets APIs-for versatile usage in different apps.",
+      "Used native Browser APIs to develop a framework agnostic abstraction over Tensorflow & ONNX AI components for use in different projects such as face-recognition and OCR.",
+      "Built  in-house  library  providing adapters for React'scomponent state to Bpmn-js XML generation.",
+      "Implemented a plugin architecture  for  extending  component  state's and creating new Bpmn elements"
+    ]
+  },
+  {
+    img: 1,
+    company: "Layermark",
+    role: "Application Developer",
+    date: "Jan 2023 - April 2023",
+    work: [
+      "Responsible  for documenting and researching areas  of improvement in an existing MFC-based app.",
+      "Replaced  a CNTK-based AI  model  with  a Yolov5-based one, and employed IPC abstractions, named pipes, sockets,and  process  forking,  for seamless yet loosely  coupled integration.",
+      "Implemented TOML-based app configurations-user specificand  default-for  more user-friendly and personalized experience.",
+      "Wrote GUI module extensions using Tkinter, andintegrated Strategies (ZMQ, Boost ASIO) for reusable andscalable Python modules"
+    ]
+  },
+  {
+    img: 2,
+    company: "Otsimo",
+    role: "Full Stack Developer",
+    date: "June 2023 - August 2023",
+    work: [
+      "Implemented  a  Go  &  gRPC microservice  for  migrating  an existing file-based ticket system to MongoDB.",
+      "Crafted a command-line migration tool for seamless transition while preserving backward compatibility.",
+      "Created  an  admin  frontend  using  React, MUI,  and  Redux, adhering to established designs for a cohesive user experience."
+    ]
   }
 ]
 
